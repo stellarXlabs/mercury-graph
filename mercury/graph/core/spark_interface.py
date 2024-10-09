ce9046ad3edd42cd72a52e9309c07409078f94bf
Francisco Moraleda <francisco.moraleda.moreno@bbva.com>
2024-10-09 17:41:43 +0200

    Set checkpoint directory for Spark

--- mercury/graph/core/spark_interface.py
+++ mercury/graph/core/spark_interface.py
@@ -74,6 +74,8 @@ class SparkInterface:
                 SparkInterface._spark_session = session
             else:
                 SparkInterface._spark_session = self._create_spark_session(config)
+                # Set checkpoint directory
+                SparkInterface._spark_session.sparkContext.setCheckpointDir(".checkpoint")
 
         if SparkInterface._graphframes is None and graphframes_installed:
             SparkInterface._graphframes = gf
