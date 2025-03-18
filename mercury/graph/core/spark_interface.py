55ab62dca97775a1c0f1e0879058ea7a5e2f7cfc
Russell Jurney <russell.jurney@gmail.com>
2025-03-18 14:42:27 -0700

    Remove commented out line that does relative path checkpoint, but leave warning

--- mercury/graph/core/spark_interface.py
+++ mercury/graph/core/spark_interface.py
@@ -72,8 +72,7 @@ class SparkInterface:
                 SparkInterface._spark_session = session
             else:
                 SparkInterface._spark_session = self._create_spark_session(config)
-                # Set checkpoint directory
-                # SparkInterface._spark_session.sparkContext.setCheckpointDir(".checkpoint")
+                # Note: must set checkpoint directory
 
         if SparkInterface._graphframes is None and graphframes_installed:
             SparkInterface._graphframes = gf
